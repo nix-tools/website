d3e6d9c778413e856e2a3d463db85d5e09257036
Simon Shine <simon@simonshine.dk>
2025-02-24 17:39:35 +0100

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Hugo Website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShellNoCC {
+      packages = [
+        pkgs.hugo
+        pkgs.just
+      ];
+    };
+  };
+}
